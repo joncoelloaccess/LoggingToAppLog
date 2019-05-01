af20641e18b317c587c4b99dbab610df61a1ad00
Jon Coello <jon.coello@theaccessgroup.com>
2019-05-01 17:29:11 +0100

    Add project files.

---
+++ LoggingToAppLog/Program.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingToAppLog
+{
+    class Program
+    {
+
+        private static ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+
+            log4net.Config.XmlConfigurator.Configure();
+            log4net.ThreadContext.Properties["EventID"] = 1022; // set to a known event id 
+
+            log.Info("information");
+            log.Warn("warning");
+            log.Error("error");
+
+            Console.ReadKey();
+
+        }
+    }
+}
